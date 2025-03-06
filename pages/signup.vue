a4834d80f27bae1167763de9eefcfb0ccd64fb32
Muhamad Hasymi Hilman <hasymimuhamad@gmail.com>
2025-03-06 10:26:34 +0700

    feat: handle undefined page, and show base component page

--- pages/signup.vue
+++ pages/signup.vue
@@ -1,3 +1,56 @@
 <template>
-  <div>signup</div>
+  <div class="bg-[grey] my-[75px] py-[75px]">
+    <div class="container text-justify h-[800px]">
+      tvshows Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam
+      dapibus turpis at neque consequat, vitae varius diam vestibulum. Donec
+      interdum bibendum lectus sagittis lobortis. Donec ac laoreet magna, in
+      consequat libero. Suspendisse potenti. Donec pellentesque in nunc sed
+      pulvinar. Mauris sed ex tristique, pharetra est id, aliquet justo. Morbi
+      at molestie mi, ut tristique mauris. Pellentesque quis risus dapibus,
+      volutpat nibh eu, gravida ipsum. Fusce ultrices urna purus, et egestas
+      augue rhoncus non. Sed sollicitudin placerat eros, eu pellentesque risus
+      egestas nec. Phasellus dignissim, sem vitae luctus tristique, mi neque
+      feugiat ipsum, nec rutrum enim enim et nibh. Aenean bibendum at massa
+      vitae cursus. Ut non risus tempus, feugiat tortor at, dictum dui. Aenean
+      sed arcu a enim iaculis pellentesque vitae et nisi. Donec a feugiat felis.
+      Phasellus ipsum risus, euismod non ornare nec, vulputate eget lacus. In
+      hac habitasse platea dictumst. Vestibulum dignissim facilisis laoreet. Ut
+      at interdum nulla, at congue sapien. Aenean eget pretium justo. Quisque
+      molestie nunc et lectus vulputate ultrices. Etiam porta porta maximus.
+      Aliquam nulla augue, fermentum ac condimentum in, varius blandit elit.
+      Nunc faucibus dui ac magna porttitor, sed mollis ante consequat.
+      Vestibulum tristique fermentum magna quis venenatis. Nullam gravida
+      consequat sem vel iaculis. Nulla sodales nisi at turpis cursus lobortis.
+      Nam volutpat magna nec purus placerat luctus. Morbi nisl est, mollis quis
+      egestas et, rhoncus in diam. Nullam eu nunc velit. Donec ultrices lorem
+      neque, a vehicula leo commodo id. Sed suscipit, dolor eget euismod
+      porttitor, tortor magna vehicula mauris, in convallis leo tortor ultricies
+      orci. Pellentesque sed interdum quam. Nullam lacinia tristique tortor,
+      facilisis vulputate purus vehicula vestibulum. In tristique laoreet
+      tortor, at mattis urna. Donec et mattis enim. Morbi rhoncus posuere lectus
+      ut fermentum. Suspendisse at lorem lobortis lorem rutrum sodales a ut
+      ligula. Aliquam interdum turpis ac tincidunt faucibus. Duis at nibh lacus.
+      Nunc dapibus ante est, pulvinar posuere felis elementum quis. Vivamus
+      neque est, fermentum in lacus at, fermentum semper lectus. Aliquam erat
+      volutpat. Cras molestie blandit ipsum, a sodales nisl euismod at. Donec id
+      hendrerit justo. Mauris eu mi volutpat, tristique justo vitae, pharetra
+      urna. Praesent ut pellentesque dolor. Nullam volutpat risus quis nulla
+      hendrerit, id placerat sapien feugiat. Nam tempus elit ut turpis
+      ultricies, id commodo diam convallis. Duis laoreet tortor sed dui mollis,
+      aliquam malesuada nibh lacinia. Aliquam a ullamcorper odio, non
+      condimentum tellus. Aliquam eget consequat dui. Vestibulum tristique sem
+      eu nulla euismod sollicitudin. Nunc sit amet turpis sit amet elit rhoncus
+      ultricies. In convallis, tortor sed semper iaculis, orci elit varius
+      felis, at congue augue eros eget lectus. Mauris scelerisque eleifend massa
+      vulputate posuere. Donec vestibulum placerat nibh ut sagittis. Mauris
+      eleifend enim nec ex gravida laoreet. Maecenas auctor quam ac ex bibendum,
+      vitae aliquet felis suscipit. Ut vulputate consequat est, sit amet iaculis
+      felis semper ut. Integer id risus id turpis tempus fringilla. Morbi augue
+      ligula, iaculis vitae varius vitae, suscipit tristique magna. Maecenas
+      odio magna, hendrerit ut nulla ac, molestie fringilla eros. Integer
+      iaculis at ligula a euismod. In varius mi ligula, id posuere elit luctus
+      vel. Nam fringilla lacinia imperdiet. Donec quis mi a tortor finibus
+      interdum.
+    </div>
+  </div>
 </template>
